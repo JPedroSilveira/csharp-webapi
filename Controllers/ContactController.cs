fec981530188526857dbb1c43c7d23c25fa497bd
João Pedro <jpedross1999@gmail.com>
2021-05-30 12:13:34 -0300

    Feat(Controller): Define and implements delete function

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using csharp_webapi.Services;
@@ -31,7 +32,7 @@ namespace csharp_webapi.Controllers
         [HttpGet("{id}")]
         [ProducesResponseType(typeof(Contact), 200)]
         [ProducesResponseType(typeof(NotFoundResult), 404)]
-        public ActionResult<Contact> GetById(int id)
+        public ActionResult<Contact> GetById([Required] int id)
         {
             var contact = _service.FindById(id);
             if (contact != null) {
@@ -51,6 +52,21 @@ namespace csharp_webapi.Controllers
             _logger.LogInformation("save contact");
             var newId = _service.Save(model.ToContact());
             return Ok(newId);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(EmptyResult), 200)]
+        [ProducesResponseType(typeof(NotFoundResult), 404)]
+        public ActionResult<EmptyResult> Delete([Required] int id)
+        {
+            var success = _service.Delete(id);
+            if (success) {
+                _logger.LogInformation($"delete contact {id}");
+                return Ok();
+            }
+
+            _logger.LogInformation($"fail to delete contact {id}");
+            return NotFound();
         }
     }
 }
